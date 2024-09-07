55a324c61e0891d2924cdc329877db66d1e8ca37
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-07 20:03:42 +0700

    Move component into dashboard folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <p>bambang</p> {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
